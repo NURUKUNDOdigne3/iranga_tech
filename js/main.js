3b2fe2d2dc35299cb08fa792e51c47dd9a052715
NURUKUNDOdigne3 <cnurukundo@gmail.com>
2024-09-24 15:16:46 +0200

    yes_true

--- js/main.js
+++ js/main.js
@@ -39,32 +39,6 @@
         return false;
     });
 
-
-    // Testimonials carousel
-    $(".testimonial-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1000,
-        margin: 25,
-        dots: false,
-        loop: true,
-        center: true,
-        responsive: {
-            0:{
-                items:1
-            },
-            576:{
-                items:1
-            },
-            768:{
-                items:2
-            },
-            992:{
-                items:3
-            }
-        }
-    });
-
-
    
 })(jQuery);
 
